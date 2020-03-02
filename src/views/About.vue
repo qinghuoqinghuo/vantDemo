c455360d7f5d4155751f8b3a3d1b27e80a696eed
lijianqiang <1058148367@qq.com>
2020-03-02 21:36:20 +0800

    创建vant项目

---
+++ src/views/About.vue
@@ -0,0 +1,15 @@
+<template>
+  <!--<div class="about">-->
+    <!--<h1>This is an about page</h1>-->
+  <!--</div>-->
+  <Carousel></Carousel>
+</template>
+<script>
+    import Carousel from '@/components/carousel.vue'
+    export default {
+        name: 'about',
+        components: {
+            Carousel
+        }
+    }
+</script>
