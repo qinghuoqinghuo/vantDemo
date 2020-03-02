37f854f86463cc3781931e28399ece6a709f08b5
lijianqiang <1058148367@qq.com>
2020-03-02 23:13:04 +0800

    修改路由

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,17 +1,12 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+// import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: Home
-  },
-  {
-    path: '/about',
     name: 'about',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
